0187e95f11b502c7984f2a887efd3a446f4cc141
LuanNguyen <huuluanpy@gmail.com>
2023-06-29 13:07:15 +0700

    add datetimeHelper

---
+++ src/Desktop.IntegrationTest/IntegrationTestCases/LGVN/Helper/DateTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDS.WITSMLstudio.Desktop.IntegrationTestCases.LGVN.Helper
+{
+    public static class DateTimeHelper
+    {
+        public static long GetCurrentDateTimeOffset() 
+        {
+            return DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        }
+
+        public static string GetCurrentDate()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
