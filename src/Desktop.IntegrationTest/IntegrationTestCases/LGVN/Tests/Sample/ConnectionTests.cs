c55e71c0250134301f6d319f052e5b8356a0f6f6
DuongLuong <duongls60149@gmail.com>
2023-07-05 14:35:23 +0700

    fix: fix type and update report tool

--- src/Desktop.IntegrationTest/IntegrationTestCases/LGVN/Tests/Sample/ConnectionTests.cs
+++ src/Desktop.IntegrationTest/IntegrationTestCases/LGVN/Tests/Sample/ConnectionTests.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PDS.WITSMLstudio.Desktop.IntegrationTestCases.LGVN.Tests.Sapmle
+namespace PDS.WITSMLstudio.Desktop.IntegrationTestCases.LGVN.Tests.Sample
 {
     [TestClass]
     public class ConnectionTests : TestBase
